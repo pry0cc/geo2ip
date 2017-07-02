30a3a38b1d938e9614a03521b7f33095305767e3
pry0cc <pry0cc@protonmail.com>
2017-07-02 20:43:52 +0000

    Just because they don't mean anything, doesn't mean you can judge

--- geo2ip.py
+++ geo2ip.py
@@ -3,12 +3,14 @@
 import GeoIP
 import reverse_geocoder as rg
 import csv
+import json
 
+def getCountryByCoord(lat, lng):
+    results = str(rg.search((lat,lng)))
+    print results
 
-gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
+#gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
+#res = gi.record_by_addr("185.65.53.155")
+#print str(res['latitude']) + "," + str(res['longitude'])
 
-print rg.search(coordinates) 
-
-res = gi.record_by_addr("185.65.53.155")
-print str(res['latitude']) + "," + str(res['longitude'])
-
+getCountryByCoord("40.151371","-3.409015")
