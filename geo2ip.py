91cc687f2005a4c8bfbefe01933889d248007009
pry0cc <pry0cc@protonmail.com>
2017-07-02 20:37:44 +0000

    stop judging

--- geo2ip.py
+++ geo2ip.py
@@ -2,8 +2,12 @@
 
 import GeoIP
 import reverse_geocoder as rg
+import csv
+
 
 gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
+
+print rg.search(coordinates) 
 
 res = gi.record_by_addr("185.65.53.155")
 print str(res['latitude']) + "," + str(res['longitude'])
