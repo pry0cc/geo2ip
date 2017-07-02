f324ba5ae37be81f1a26d7d873ad6e8d7156cb1b
pry0cc <pry0cc@protonmail.com>
2017-07-02 20:21:44 +0000

    added some stuff

--- city.py
+++ city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 
 import GeoIP
+import reverse_geocoder as rg
 
 gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 
-print gi.record_by_addr("10.10.10.10")
+print gi.record_by_addr("123.45.23.14")
 
