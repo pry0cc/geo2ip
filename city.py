e85de55d83370fb71bd4e22cff1a049da4515a5e
IoTh1nkN0t <unh0lys0daz@gmail.com>
2017-07-02 22:21:39 +0200

    Google maps ready format

--- city.py
+++ city.py
@@ -5,6 +5,5 @@ import GeoIP
 gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 
 res = gi.record_by_addr("185.65.53.155")
-print res['longitude']
-print res['latitude']
+print str(res['latitude']) + "," + str(res['longitude'])
 
