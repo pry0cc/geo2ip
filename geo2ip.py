c1b9d85144a5e377d19dba5b4696f1e772102dbc
pry0cc <pry0cc@protonmail.com>
2017-07-15 11:46:25 +0000

    Made major improvements, more efficient sorting, easier to pars

--- geo2ip.py
+++ geo2ip.py
@@ -5,6 +5,7 @@ import reverse_geocoder as rg
 import csv
 import argparse as ag
 import math
+import socket, struct
 
 parser = ag.ArgumentParser(description="Find IP from GEO Location")
 parser.add_argument('lat', type=float, help='latitude of target')
@@ -13,7 +14,6 @@ parser.add_argument('lon', type=float, help='longitude of target')
 args = parser.parse_args()
 
 def ips(start, end):
-    import socket, struct
     start = struct.unpack('>I', socket.inet_aton(start))[0]
     end = struct.unpack('>I', socket.inet_aton(end))[0]
     return [socket.inet_ntoa(struct.pack('>I', i)) for i in range(start, end)]
@@ -28,23 +28,47 @@ def IPFromCC(cc):
         data.append(row[0:2])
     return data
 
-def IPFromCoord(lat, lon, radius):
+def simpleForm(val):
+    # Take input as float.
+    if str(val).split(".")[1] == "0":
+        return str(int(val))
+    else:
+        return str(val)
+
+def shortenIP(ip):
+    result = ""
+    octets = ip.split(".")[0:3]
+    for octet in octets:
+        if octets.index(octet) < 2:
+            result += octet + "."
+        else:
+            result += octet + ".1"
+    return result
+
+def IPFromCoord(lat, lon):
     gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
     cc = getCountryByCoord(lat,lon)
-    results = IPFromCC(cc)
-    for ip_range in results:
-        print "Starting new range"
-        for ip in ips(ip_range[0], ip_range[1]):
-            res = gi.record_by_addr(ip)
-            ip_lat = str(float(math.floor(res["latitude"] * 1000) / 1000))
-            ip_lon = str(float(math.floor(res["longitude"] * 1000) / 1000))
-            
-            if (str(lat) in ip_lat) and (str(lon) in ip_lon):
-                print ip + ": " + ip_lat + ", " + ip_lon
 
+    results = IPFromCC(cc)
+
+    try:
+        for ip_range in results:
+            ips_basic = []
+            for ip in ips(ip_range[0], ip_range[1]):
+                ips_basic.append(shortenIP(ip))
+
+            for ip in sorted(set(ips_basic)):
+                res = gi.record_by_addr(ip)
+                ip_lat = str(float(math.floor(res["latitude"] * 1000) / 1000))
+                ip_lon = str(float(math.floor(res["longitude"] * 1000) / 1000))
+
+                if (simpleForm(lat) in ip_lat) and (simpleForm(lon) in ip_lon):
+                    print shortenIP(ip) + ": " + ip_lat + ", " + ip_lon + " " + str(res["city"])
+
+    except:
+        pass
 #gi = GeoIP.open("GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 #res = gi.record_by_addr("185.65.53.155")
 #print str(res['latitude']) + "," + str(res['longitude'])
 
-print IPFromCoord(args.lat, args.lon, 3)
-
+print IPFromCoord(args.lat, args.lon)
